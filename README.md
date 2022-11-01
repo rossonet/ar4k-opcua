e999e9faa792b8877d64561ad4d6756094746619
Cooperativa Rossonet <rossonet@users.noreply.github.com>
2022-11-01 15:49:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Template Library Rossonet
+
+[![Test on master branch with Gradle](https://github.com/rossonet/TemplateCommons/actions/workflows/test-on-master-with-gradle.yml/badge.svg)](https://github.com/rossonet/TemplateCommons/actions/workflows/test-on-master-with-gradle.yml)
+
+![alt text](https://app.rossonet.net/wp-content/uploads/2021/10/rossonet-logo_280_115.png "Rossonet")
+
+[https://www.rossonet.net](https://www.rossonet.net)
+
---
+++ src/test/java/org/rossonet/tests/MainTest.java
@@ -0,0 +1,30 @@
+package org.rossonet.tests;
+
+import java.util.logging.Logger;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.rossonet.main.Main;
+
+@TestMethodOrder(OrderAnnotation.class)
+public class MainTest {
+
+	private static final Logger logger = Logger.getLogger(MainTest.class.getName());
+
+	@AfterEach
+	public void cleanTestsContext() throws Exception {
+		logger.info("test completed");
+	}
+
+	@Test
+	@Order(1)
+	public void runMain() throws Exception {
+		final String[] args = new String[] {};
+		Main.main(args);
+		logger.info("ok");
+	}
+
+}
